154c4e82b7e5754e108126c76a54fa96697d54c5
buzamal <buzamal@urbandaleschools.com>
2025-09-04 10:29:18 -0500

    commit

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,10 +1,10 @@
 '''
-1.0 Jedi Training (17pts)  Name:________________
+1.0 Jedi Training (17pts)  Name:malachi buza
 
 
-1. Define Forking (1pt): 
+1. Define Forking (1pt): coping a repository from anothers github account to ones own account
 
-2. Define Cloning (1pt): 
+2. Define Cloning (1pt): downloading a repository to my pc from my github to work
 
 3. Define Branching (1pt):
 
@@ -14,7 +14,7 @@
 
 6. Define Pushing (1pt):
 
-7. Define Pull Request (1pt):
+7. Define Pull Request (1pt): a request to pull a piece of code into the big project on git
 
 
 8. TURTORIAL ART (10pts.)
@@ -26,28 +26,18 @@ The last line keeps the window open until you click to close.
 Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=turtle
 '''
 import turtle
+
+
+
 yoda=turtle.Turtle()
 screen=turtle.Screen() # makes a screen object
 screen.bgcolor('black') # colors the screen
 yoda.pensize(3) # width of pen line
 yoda.speed(10)  # speed of drawing. Go fast to not waste time.
 yoda.color("#00FF00")
-yoda.circle(100)  #head
 yoda.penup()
-yoda.setpos(50,185) #right ear
-yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
-yoda.penup()
-yoda.setpos(-50,185)  #left ear
-yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
-yoda.penup()
-yoda.setpos(200,-300)
-yoda.pendown()
-yoda.pencolor('#00FF00')
+yoda.forward(50)
 
 
-yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
+yoda.write('malachi',font=("Arial", 20, "normal")) # signs your name to your art
 turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
